e45c3e5581b40d326c2fd3c4277174d625a25478
k3ys5r0k3 <bryandonnelly7@gmail.com>
2020-02-03 12:10:38 +0000

    .

---
+++ src/JSONHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Com_Challenge.src
+{
+    public class JSONHandler
+    {
+        public static void FileReader(string filename, object type)
+        {
+
+        }
+
+        public static void ReplaceJsonValue()
+        {
+            string filepath = "../../json1.json";
+            string result = string.Empty;
+            using (StreamReader r = new StreamReader(filepath))
+            {
+                var json = r.ReadToEnd();
+                var jobj = JObject.Parse(json);
+                foreach (var item in jobj.Properties())
+                {
+                    item.Value = item.Value.ToString().Replace("v1", "v2");
+                }
+                result = jobj.ToString();
+                Console.WriteLine(result);
+            }
+            File.WriteAllText(filepath, result);
+        }
+    }
+}
