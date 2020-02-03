0efbbffad994601d32ccf2a643329375385a113e
ciaran <vavasour1@gmail.com>
2020-02-03 13:42:47 +0000

    player class update

--- src/Player.cs
+++ src/Player.cs
@@ -6,8 +6,50 @@ namespace Com_Challenge.Classes.Character
 {
     class Player
     {
-        public string name { get; set; }
-        public string gender { get; set; }
-        public int health { get; set; }
+
+        public Player(string name, string gender, int health)
+        {
+            this.name = name;
+            this.gender = gender;
+            this.health = health;
+
+        }
+        //construstor 
+        public Player()
+        {
+
+        }
+        //setters 
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        public void SetGender(string gender)
+        {
+            this.gender = gender;
+        }
+
+        //Getters 
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        public string GetGender()
+        {
+            return this.gender;
+        }
+
+        public int GetHealth()
+        {
+            return this.health;
+        }
+
+
+        public void print()
+        {
+            Console.WriteLine("character: " + this.name + "/nGender: " + this.gender + "/nHealth: " + health + "./n");
+        }
     }
 }
