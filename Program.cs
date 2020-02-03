2d966c59513b54f5860fd6edaa5e069a9c8feb62
k3ys5r0k3 <bryandonnelly7@gmail.com>
2020-02-03 11:40:45 +0000

    Fixed repo

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Com_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/Player.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com_Challenge.Classes.Character
+{
+    class Player
+    {
+    }
+}
