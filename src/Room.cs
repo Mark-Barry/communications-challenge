7a9ffa514c19a297f756d7fe3d177528fd4dbaf6
k3ys5r0k3 <bryandonnelly7@gmail.com>
2020-02-04 09:46:02 +0000

    Renamed doors class and added List<Door> to Room class

--- src/Room.cs
+++ src/Room.cs
@@ -1,11 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using Com_Challenge.src;
 
 public class Room
 {
+
     public string Name { get; set; }
     public string Description { get; set; }
+    public List<Door> DoorList;
 
-    public Room()
-	{
-	}
+    public Room(string name, string description)
+    {
+        this.Name = name;
+        this.Description = description;
+        DoorList = new List<Door>();
+    }
 }
