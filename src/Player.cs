0063c5172ed171cbfd577085acf6e00df00e10c4
ciaran <vavasour1@gmail.com>
2020-02-04 12:08:53 +0000

    added some properties to player class

--- src/Player.cs
+++ src/Player.cs
@@ -18,7 +18,8 @@ namespace Com_Challenge.Classes.Character
         public string Name { get { return this.GetName(); } set { this.SetName(Name); } }
         public string Gender { get { return this.GetGender(); } set { this.SetGender(Gender); } }
         public int Health { get { return this.GetHealth(); } set { this.SetHealth(Health); } }
-
+        public string CurrentLocation { get { return this.GetCurrenetLocation(); } set { this.SetCurrenetLocation(); } }
+        public string PreviousLocation { get { return this.GetPreviousLocation(); } set { this.SetPreviousLocation(); } }
 
         public Player(string name, string gender, int health, Inventory inventory)
         {
